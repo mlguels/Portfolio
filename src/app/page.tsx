3b858114266f16be3d224d0221ebb04483514256
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-04-18 08:57:32 -0700

    feat: Added about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
+import About from './components/About';
 
 export default function Home() {
 	return (
@@ -10,6 +11,7 @@ export default function Home() {
 			</nav>
 			<section>
 				<Main />
+				<About />
 			</section>
 		</main>
 	);
