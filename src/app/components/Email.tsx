d7824f4d566c4acb6f3b18006e252d08853f68f7
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-05-17 12:55:11 -0700

    Added Copy2Clipboard Functionality

--- src/app/components/Email.tsx
+++ src/app/components/Email.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import Image from 'next/image';
+'use client';
 
-const Email = () => (
-	<div className='bg-[#ffff] rounded-lg shadow-xl shadow-[#7caad3]  cursor-pointer hover:scale-125 ease-in duration-200'>
-		<Image src='/assets/email.svg' width={45} height={45} alt='email'/>
-	</div>
-);
+import React, {useState} from 'react';
+import Image from 'next/image';
+import copy from 'clipboard-copy';
+import {AiOutlineCheckCircle} from 'react-icons/ai';
+
+const Email = () => {
+	const [showCheck, setShowCheck] = useState(false);
+
+	const handleClick = () => {
+		const email = 'mrodandres@icloud.com';
+		copy(email);
+		setShowCheck(true);
+		setTimeout(() => {
+			setShowCheck(false);
+		}, 3000);
+	};
+
+	return (
+		<div className='bg-[#ffff] rounded-lg cursor-pointer hover:scale-125 ease-in duration-200'>
+			<button onClick={handleClick}>
+				<Image src='/svg/email.svg' width={45} height={45} alt='email'/>
+			</button>
+			<div className='pl-2 text-sky-300'>
+				{showCheck && (
+					<ul className='flex'>
+						<li className='font-light'>Email Copied!</li>
+						<li><AiOutlineCheckCircle size={25} /></li>
+					</ul>
+				)}
+			</div>
+		</div>
+	);
+};
 
 export default Email;
